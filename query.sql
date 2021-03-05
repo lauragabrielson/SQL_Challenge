c2a576a77f8f0b73ec8217d3cc1981fc399d5481
lauragabrielson <lauramwinge@hotmail.com>
2021-03-04 22:16:10 -0600

    Tables

--- query.sql
+++ query.sql
@@ -12,7 +12,14 @@ from "Employees"
 where hire_date >= '1986-01-01' and 
 hire_date <= '1986-12-31';
 
-
+--3 List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+from "Employees" as e
+inner join "Department Manager" as dm
+	on e.emp_no=dm.emp_no
+inner join "Departments" as d
+	on dm.dept_no=d.dept_no;
+	
 
 --6
 select e.emp_no, e.last_name, e.first_name, d.dept_name
