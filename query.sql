ada165983f867211edb83153ddcf88133e2d984a
lauragabrielson <lauramwinge@hotmail.com>
2021-03-06 12:57:41 -0600

    Final

--- query.sql
+++ query.sql
@@ -1,12 +1,12 @@
 select * from "Salaries"
 
---1
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from "Salaries" as s
 inner join "Employees" as e on
 e.emp_no=s.emp_no;
 
---2
+--2. List first name, last name, and hire date for employees who were hired in 1986.
 select first_name, last_name, hire_date
 from "Employees"
 where hire_date >= '1986-01-01' and 
@@ -20,8 +20,23 @@ inner join "Department Manager" as dm
 inner join "Departments" as d
 	on dm.dept_no=d.dept_no;
 	
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "Employees" as e
+inner join "Department Employees" as de
+	on de.emp_no=e.emp_no
+inner join "Departments" as d
+	on de.dept_no=d.dept_no;
+	
+	
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
---6
+select first_name, last_name, sex
+from "Employees"
+where first_name = 'Hercules' and
+last_name like 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 select e.emp_no, e.last_name, e.first_name, d.dept_name
 from "Employees" as e
 inner join "Department Employees" as de
@@ -29,8 +44,26 @@ inner join "Department Employees" as de
 inner join "Departments" as d
 	on de.dept_no=d.dept_no
 	where d.dept_name = 'Sales'
-	order by e.emp_no
+	order by e.emp_no;
 
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "Employees" as e
+inner join "Department Employees" as de
+	on e.emp_no=de.emp_no
+inner join "Departments" as d
+	on de.dept_no=d.dept_no
+	where d.dept_name = 'Sales' or
+	d.dept_name = 'Development'
+	order by e.emp_no;
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+select last_name, count(last_name)
+from "Employees"
+group by last_name 
+order by last_name desc;
 
 
 
